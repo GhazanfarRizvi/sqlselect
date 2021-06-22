781ef365bf51e59f35df1c01e107c2f9c1cd8d30
Syed Ghazanfar Ali Rizvi <68903@omantel.om>
2021-06-23 01:15:52 +0400

    Removed unused func and added a comment with Select func

--- select.go
+++ select.go
@@ -5,16 +5,8 @@ import(
 	"fmt"
 ) 
 
+//SQL SELECT command for all tables
 func Select(db *sql.DB, sel string) (selectRows *sql.Rows) {
-	selectRows, err := db.Query(sel)
-	if err != nil {
-		fmt.Println("Error running ODM_FWA_SUBS_TRAFFIC_4GAVG select query")
-		fmt.Println(err)
-	}
-	return selectRows
-}
-
-func Odmfwasubsmaster1(db *sql.DB, sel string) (selectRows *sql.Rows) {
 	selectRows, err := db.Query(sel)
 	if err != nil {
 		fmt.Println("Error running ODM_FWA_SUBS_TRAFFIC_4GAVG select query")
